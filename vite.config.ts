e7e1c312177ee5bc83726daa66afeb473d9eab8b
Isabel Paredes <isabel.paredes@quantstack.net>
2025-09-22 17:12:30 +0200

    Clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,5 @@
-import { defineConfig } from 'vite';
-import copy from 'rollup-plugin-copy';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: '.',
-  base: 'https://isabelparedes.github.io/pydata2025/',
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      plugins: [
-        copy({
-          targets: [{ src: 'slides.md', dest: 'dist' }],
-          hook: 'writeBundle',
-        }),
-      ],
-    },
-  },
+  base: "https://isabelparedes.github.io/pydata2025/",
 });
